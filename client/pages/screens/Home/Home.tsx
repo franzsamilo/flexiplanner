1eeea426cf753884b32d73738652cba7e7f412e3
franzsamilo <franzeliezer.samilo-22@cpu.edu.ph>
2023-11-22 21:44:54 +0800

    [Adjustments][Sam] Changed text display for Home

--- client/pages/screens/Home/Home.tsx
+++ client/pages/screens/Home/Home.tsx
@@ -16,7 +16,7 @@ function Home() {
         className="w-48 block bg-blue-500 hover:bg-blue-400 focus:bg-blue-400 text-white font-semibold rounded-lg
                 px-4 py-3 mt-6"
       >
-        Go to Category Page
+        Category Page
       </button>
     </div>
   );
