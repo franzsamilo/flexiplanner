cd5e62ae5580ba6cbfca58fd0650effd0dd2baa9
franzsamilo <franzeliezer.samilo-22@cpu.edu.ph>
2023-11-22 18:47:57 +0800

    [Routing][Sam] Page Routing for Screens

---
+++ client/pages/screens/CategoryView/CategoryPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useRouter } from "next/router";
+
+function CategoryPage() {
+  return (
+    <div>
+      Dis da Category Page 
+    </div>
+  )
+}
+
+export default CategoryPage
--- client/pages/screens/Home/Home.tsx
+++ client/pages/screens/Home/Home.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import { useRouter } from "next/router";
 
 function Home() {
+  const router = useRouter();
+
+  const navigateToCategory = () => {
+    router.push("/screens/CategoryView/CategoryPage");
+  };
+
   return (
-    <div>
+    <div className="flex-col">
       This the home page fr
+      <button
+        onClick={navigateToCategory}
+        className="w-48 block bg-blue-500 hover:bg-blue-400 focus:bg-blue-400 text-white font-semibold rounded-lg
+                px-4 py-3 mt-6"
+      >
+        Go to Category Page
+      </button>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
