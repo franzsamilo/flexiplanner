6c3303c02c02a4ffd379239378b73cf065c94eb9
franzsamilo <franzeliezer.samilo-22@cpu.edu.ph>
2023-11-24 00:46:17 +0800

    [Feature][Sam] User Authentication

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,18 +1,26 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import { useUser } from "@auth0/nextjs-auth0/client";
+
+import Home from "./screens/Home/Home";
 import Image from "next/image";
 import splash from "../public/assets/splash.jpg";
-import google from "../public/assets/icons/google.svg";
-
+import splash2 from "../public/assets/splash2.jpg";
 
 function index() {
   const router = useRouter();
+  const { user, error, isLoading } = useUser();
+
   const [message, setMessage] = useState("Loading");
 
-  const navigateToHome = () => {
-    router.push("/screens/Home/Home");
-  };
+  function Logout() {
+    router.push("/api/auth/logout");
+  }
+
+  function Login() {
+    router.push("/api/auth/login");
+  }
 
   useEffect(() => {
     fetch("http://localhost:6969/api/login")
@@ -23,6 +31,18 @@ function index() {
       });
   }, []);
 
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
+
+  if (user) {
+    return (
+      <div className="w-screen h-screen overflow-y-scroll">
+        <Home />
+        <button onClick={Logout}>Logout</button>
+      </div>
+    );
+  }
+
   return (
     <div className="flex flex-col md:flex-row h-screen items-center">
       <div className="bg-blue-600 hidden lg:block w-full md:w-1/2 xl:w-2/3 h-screen">
@@ -31,80 +51,35 @@ function index() {
 
       <div
         className="bg-white w-full md:max-w-md lg:max-w-full md:mx-auto md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12
-          flex items-center justify-center"
+        flex items-center justify-center"
       >
-        <div className="w-full h-100">
-          <h1 className="text-xl font-bold">{message}</h1>
-          <h1 className="text-xl md:text-2xl font-bold leading-tight mt-12">
+        <div className="w-full h-100 items-center text-center ">
+          <h1 className="text-3xl font-bold text-blue-300 mb-12">{message}</h1>
+          <Image src={splash2} alt="" className="rounded-3xl"/>
+          <h1 className="text-xl md:text-2xl font-bold leading-tight mt-12  text-blue-700">
             Log in to your account
           </h1>
-
-          <form className="mt-6" action="#" method="POST">
-            <div>
-              <label className="block text-gray-700">Email Address</label>
-              <input
-                type="email"
-                name=""
-                id=""
-                placeholder="Enter Email"
-                className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none"
-                required
-              />
-            </div>
-
-            <div className="mt-4">
-              <label className="block text-gray-700">Password</label>
-              <input
-                type="password"
-                name=""
-                id=""
-                placeholder="Enter Password"
-                className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500
-                  focus:bg-white focus:outline-none"
-                required
-              />
-            </div>
-
-            <div className="text-right mt-2">
-              <a
-                href="#"
-                className="text-sm font-semibold text-gray-700 hover:text-blue-700 focus:text-blue-700"
-              >
-                Forgot Password?
-              </a>
-            </div>
-
-            <button
-              type="submit"
-              onClick={navigateToHome}
-              className="w-full block bg-blue-500 hover:bg-blue-400 focus:bg-blue-400 text-white font-semibold rounded-lg
-                px-4 py-3 mt-6"
-            >
-              Log In
-            </button>
-          </form>
 
           <hr className="my-6 border-gray-300 w-full" />
 
           <button
-            type="button"
-            onClick={navigateToHome}
-            className="w-full block bg-white hover:bg-gray-100 focus:bg-gray-100 text-gray-900 font-semibold rounded-lg px-4 py-3 border border-gray-300"
+            onClick={Login}
+            className="w-full block bg-blue-500 hover:bg-blue-400 focus:bg-blue-400 text-white font-semibold rounded-lg
+              px-4 py-3 mt-6"
           >
-            <div className="flex items-center justify-center">
-              <Image src={google} alt="" className="w-6 h-6" />
-              <span className="ml-4">Log in with Google</span>
-            </div>
+            Login
           </button>
+
+          <hr className="my-6 border-gray-300 w-full" />
 
           <p className="mt-8">
             Need an account?
-            <a
-              href="#"
+            <button
+              onClick={Login}
               className="text-blue-500 hover:text-blue-700 font-semibold ml-3"
             >
               Create an account
-            </a>
+            </button>
           </p>
 
           <p className="text-sm text-gray-500 mt-12">
--- client/pages/screens/Home/Home.tsx
+++ client/pages/screens/Home/Home.tsx
@@ -4,20 +4,11 @@ import { useRouter } from "next/router";
 function Home() {
   const router = useRouter();
 
-  const navigateToCategory = () => {
-    router.push("/screens/CategoryView/CategoryPage");
-  };
+
 
   return (
     <div className="flex-col">
       This the home page fr
-      <button
-        onClick={navigateToCategory}
-        className="w-48 block bg-blue-500 hover:bg-blue-400 focus:bg-blue-400 text-white font-semibold rounded-lg
-                px-4 py-3 mt-6"
-      >
-        Go to Category Page
-      </button>
     </div>
   );
 }
