1ea71b08b5530d27781aea80d8b5955d29023546
franzsamilo <franzeliezer.samilo-22@cpu.edu.ph>
2023-11-23 21:39:56 +0800

    [Adjustment][Sam] remove unused navigation in home

--- client/pages/screens/Home/Home.tsx
+++ client/pages/screens/Home/Home.tsx
@@ -4,10 +4,6 @@ import { useRouter } from "next/router";
 function Home() {
   const router = useRouter();
 
-  const navigateToCategory = () => {
-    router.push("/screens/CategoryView/CategoryPage");
-  };
-
   return (
     <div className="flex-col">
       This the home page fr
