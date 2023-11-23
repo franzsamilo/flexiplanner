5709ed78e71973481e6decc57aef282cfd5ccd59
franzsamilo <franzeliezer.samilo-22@cpu.edu.ph>
2023-11-23 21:18:10 +0800

    [UI][Sam] UI Temp for screens + routing

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -5,13 +5,16 @@ import Image from "next/image";
 import splash from "../public/assets/splash.jpg";
 import google from "../public/assets/icons/google.svg";
 
-
 function index() {
   const router = useRouter();
   const [message, setMessage] = useState("Loading");
 
   const navigateToHome = () => {
     router.push("/screens/Home/Home");
+  };
+
+  const navigateToSignUP = () => {
+    router.push("/screens/GetStarted/GetStarted");
   };
 
   useEffect(() => {
@@ -35,56 +38,9 @@ function index() {
       >
         <div className="w-full h-100">
           <h1 className="text-xl font-bold">{message}</h1>
-          <h1 className="text-xl md:text-2xl font-bold leading-tight mt-12">
+          <h1 className="text-xl md:text-2xl font-bold leading-tight mt-12 mb-12">
             Log in to your account
           </h1>
-
-          <form className="mt-6" action="#" method="POST">
-            <div>
-              <label className="block text-gray-700">Email Address</label>
-              <input
-                type="email"
-                name=""
-                id=""
-                placeholder="Enter Email"
-                className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none"
-                required
-              />
-            </div>
-
-            <div className="mt-4">
-              <label className="block text-gray-700">Password</label>
-              <input
-                type="password"
-                name=""
-                id=""
-                placeholder="Enter Password"
-                className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500
-                  focus:bg-white focus:outline-none"
-                required
-              />
-            </div>
-
-            <div className="text-right mt-2">
-              <a
-                href="#"
-                className="text-sm font-semibold text-gray-700 hover:text-blue-700 focus:text-blue-700"
-              >
-                Forgot Password?
-              </a>
-            </div>
-
-            <button
-              type="submit"
-              onClick={navigateToHome}
-              className="w-full block bg-blue-500 hover:bg-blue-400 focus:bg-blue-400 text-white font-semibold rounded-lg
-                px-4 py-3 mt-6"
-            >
-              Log In
-            </button>
-          </form>
-
-          <hr className="my-6 border-gray-300 w-full" />
 
           <button
             type="button"
@@ -97,14 +53,25 @@ function index() {
             </div>
           </button>
 
+          <hr className="my-6 border-gray-300 w-full" />
+
+          <button
+            type="submit"
+            onClick={navigateToHome}
+            className="w-full block bg-blue-500 hover:bg-blue-400 focus:bg-blue-400 text-white font-semibold rounded-lg
+                px-4 py-3 mt-6"
+          >
+            Continue Offline
+          </button>
+
           <p className="mt-8">
             Need an account?
-            <a
-              href="#"
+            <button
+              onClick={navigateToSignUP}
               className="text-blue-500 hover:text-blue-700 font-semibold ml-3"
             >
               Create an account
-            </a>
+            </button>
           </p>
 
           <p className="text-sm text-gray-500 mt-12">
--- client/pages/screens/Home/Home.tsx
+++ client/pages/screens/Home/Home.tsx
@@ -11,13 +11,6 @@ function Home() {
   return (
     <div className="flex-col">
       This the home page fr
-      <button
-        onClick={navigateToCategory}
-        className="w-48 block bg-blue-500 hover:bg-blue-400 focus:bg-blue-400 text-white font-semibold rounded-lg
-                px-4 py-3 mt-6"
-      >
-        Category Page
-      </button>
     </div>
   );
 }
