be189b287e1c13cd1a8ae0ac6ba4db860655ddab
franzsamilo <franzeliezer.samilo-22@cpu.edu.ph>
2023-12-30 17:25:41 +0800

    [Feature][Sam] Database Migration & Connection

---
+++ server/db/migrations/20231230080853_create_users_table.sql
@@ -0,0 +1,11 @@
+-- migrate:up
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- migrate:down
+DROP TABLE users;
