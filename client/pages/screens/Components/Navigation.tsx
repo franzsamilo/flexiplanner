ca7cfc8429e5d488fb5021ebf44ca328fe57c1b2
itchydimeee <124387580+itchydimeee@users.noreply.github.com>
2024-01-03 22:26:27 +0800

    [routing][zyd] routing update and some minor adjustments

--- client/pages/screens/Components/Navigation.tsx
+++ client/pages/screens/Components/Navigation.tsx
@@ -1,29 +1,32 @@
-import { useRouter } from "next/router";
+import { useRouter } from 'next/router'
 
-function useNavigation() {
-  const router = useRouter();
-  function navigateTo(path: string) {
-    router.push(path);
+function useNavigation () {
+  const router = useRouter()
+  function navigateTo (path: string) {
+    router.push(path)
   }
 
-  function ToHome() {
-    navigateTo("/screens/Home/Home");
+  function ToHome () {
+    navigateTo('/screens/Home/Home')
   }
 
-  function ToAboutUs() {
-    navigateTo("/screens/AboutUs/aboutUs");
+  function ToAboutUs () {
+    navigateTo('/screens/AboutUs/AboutUs')
   }
 
-  function ToDemo() {
-    navigateTo("/screens/Demo/demo");
+  function ToDemo () {
+    navigateTo('/screens/Demo/Demo')
   }
 
-  function ToLogin() {
-    navigateTo("/screens/Login/login");
+  function ToLogin () {
+    navigateTo('/screens/Login/Login')
   }
 
-  function ToSignUp() {
-    navigateTo("/screens/SignUp/SignUp");
+  function ToSignUp () {
+    navigateTo('/screens/SignUp/SignUp')
+  }
+  function ToScheduler () {
+    navigateTo('/screens/Scheduler/Scheduler')
   }
 
   return {
@@ -32,7 +35,8 @@ function useNavigation() {
     ToDemo,
     ToLogin,
     ToSignUp,
-  };
+    ToScheduler
+  }
 }
 
-export default useNavigation;
+export default useNavigation
