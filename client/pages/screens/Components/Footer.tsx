b79ac87e179345f4d124e00473fa93adf26bf6ce
mrenzotan <markrenzo.tan-20@cpu.edu.ph>
2024-01-03 23:39:27 +0800

    [Adjustments][Renzo] responsive UI design (unfinished)

--- client/pages/screens/Components/Footer.tsx
+++ client/pages/screens/Components/Footer.tsx
@@ -1,39 +1,44 @@
-import React from 'react'
-import Image from 'next/image'
-import logo from '/public/assets/logo.png'
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
+import React from 'react';
+import Image from 'next/image';
+import logo from '/public/assets/logo.png';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
-function Footer () {
+function Footer() {
   return (
-      <footer className='bg-main text-white p-4 h-40'>
-        <div className='flex justify-between'>
-          <div className='flex items-start'>
-            <div className='mr-2'>
-              <Image src={logo} alt='' className='h-20 w-20 object-cover' />
-            </div>
-            <div className='overflow-wrap'>
-              <span className='italic text-lg font-medium max-w-xs break-words'>
-                About Us:
-              </span>
-              <h2 className='max-w-[40%]'>
-                At the core of FlexiPlanner lies innovation by the Liberal
-                Party, offering a flexible way to organize. Empowering through
-                technology for a brighter, efficient tomorrow.
-              </h2>
-            </div>
+    <footer className="bg-main text-white p-4 mt-auto">
+      <div className="flex justify-between">
+        <div className="flex flex-row items-start w-1/2">
+          <div className="flex mr-2">
+            <Image
+              src={logo}
+              alt=""
+              className="h-16 w-24 object-cover  xs:max-lg:hidden"
+            />
           </div>
-          <div className='flex flex-col justify-start ml-4'>
-            <div className='mr-4'>
-              <span className='italic text-lg font-medium'>Contact Us:</span>
-              <h1 className='text-lg font-medium'>Feel free to reach out:</h1>
-              <p>Email: liberalparty@gmail.com</p>
-              <p>Contact Number: 09696969699</p>
+          <div>
+            <div className="italic text-lg font-medium max-w-xs break-words xs:max-md:text-xs">
+              About Us:
             </div>
+            <h2 className="xs:max-lg:text-xs">
+              At the core of FlexiPlanner lies innovation by the Liberal Party,
+              offering a flexible way to organize. Empowering through technology
+              for a brighter, efficient tomorrow.
+            </h2>
           </div>
         </div>
-      </footer>
-  )
+        <div className="flex flex-col justify-start ml-4">
+          <div className="mr-4 ml-4 xs:max-md:text-xs xs:max-md:mr-2 xs:max-md:ml-2">
+            <div className="italic text-lg font-medium xs:max-md:text-xs">
+              Contact Us:
+            </div>
+            <p>Email: liberalparty@gmail.com</p>
+            <p>Contact Number: 09696969699</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
--- client/pages/screens/Home/Home.tsx
+++ client/pages/screens/Home/Home.tsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import Image from 'next/image'
-import Slider from 'react-slick'
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
-import Navbar from '../Components/Navbar'
-import Footer from '../Components/Footer'
-import useNavigation from '../Components/Navigation'
+import React from 'react';
+import Image from 'next/image';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Navbar from '../Components/Navbar';
+import Footer from '../Components/Footer';
+import useNavigation from '../Components/Navigation';
 
-function HomePage () {
-  const { ToLogin } = useNavigation()
+function HomePage() {
+  const { ToLogin } = useNavigation();
   const carouselSettings = {
     dots: true,
     infinite: true,
@@ -16,75 +16,75 @@ function HomePage () {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 5000
-  }
+    autoplaySpeed: 5000,
+  };
 
-  const sliderRef = React.useRef<Slider>(null)
+  const sliderRef = React.useRef<Slider>(null);
 
   const handlePrev = () => {
     if (sliderRef.current) {
-      sliderRef.current.slickPrev()
+      sliderRef.current.slickPrev();
     }
-  }
+  };
 
   const handleNext = () => {
     if (sliderRef.current) {
-      sliderRef.current.slickNext()
+      sliderRef.current.slickNext();
     }
-  }
+  };
 
   return (
-    <div className='flex flex-col min-h-screen bg-dirty'>
-      <Navbar/>
-      <main className='bg-gradient-to-br from-white via-blue-100 to-blue-300'>
-        <div className='mx-auto my-[10px] w-[600px] '>
-          <p className='font-raleway font-extrabold text-[#425dc8] text-5xl text-center  leading-[1.25]'>
+    <div className="flex flex-col w-full min-h-screen bg-dirty">
+      <Navbar />
+      <main className="bg-gradient-to-br from-white via-blue-100 to-blue-300">
+        <div className="mx-auto my-2 xs:max-sm:my-6">
+          <p className="font-raleway font-extrabold text-[#425dc8] text-5xl text-center  leading-[1.25] xs:max-sm:text-xl md:max-lg:text-3xl">
             A flexible way to start your day
           </p>
         </div>
 
-        <div className='my-5'>
-          <p className='font-raleway font-bold text-[#1e1e1e] text-xl text-center'>
+        <div className="my-5">
+          <p className="font-raleway font-bold text-[#1e1e1e] text-xl text-center xs:max-sm:text-xs md:max-lg:text-lg">
             Make spontaneous changes anytime, anywhere
           </p>
         </div>
 
-        <div className='flex justify-center items-center my-10'>
+        <div className="flex justify-center items-center my-10">
           <button
-            className='px-[25px] py-[14px] bg-main rounded-full font-roboto font-bold text-xl text-white'
+            className="px-[25px] py-[14px] bg-main rounded-full font-roboto font-bold text-xl text-white"
             onClick={ToLogin}
           >
             Get Started
           </button>
         </div>
 
-        <div className='flex flex-row items-center justify-center my-10'>
+        <div className="flex flex-row items-center justify-center my-10">
           <div>
             <button onClick={handlePrev}>
               <Image
-                className='w-auto px-3'
-                src='/assets/icons/arrow-left.png'
-                alt='arrow-left'
+                className="w-auto px-3"
+                src="/assets/icons/arrow-left.png"
+                alt="arrow-left"
                 width={40}
                 height={64}
               />
             </button>
           </div>
 
-          <div className='w-full max-w-3xl overflow-hidden'>
+          <div className="w-full max-w-3xl overflow-hidden">
             <Slider ref={sliderRef} {...carouselSettings}>
               <div>
                 <Image
-                  src='/assets/image1.png'
-                  alt='Image 1'
+                  src="/assets/image1.png"
+                  alt="Image 1"
                   width={1200}
                   height={600}
                 />
               </div>
               <div>
                 <Image
-                  src='/assets/image2.png'
-                  alt='Image 2'
+                  src="/assets/image2.png"
+                  alt="Image 2"
                   width={1200}
                   height={600}
                 />
@@ -95,9 +95,9 @@ function HomePage () {
           <div>
             <button onClick={handleNext}>
               <Image
-                className='w-auto px-3'
-                src='/assets/icons/arrow-right.png'
-                alt='arrow-right'
+                className="w-auto px-3"
+                src="/assets/icons/arrow-right.png"
+                alt="arrow-right"
                 width={40}
                 height={64}
               />
@@ -105,9 +105,9 @@ function HomePage () {
           </div>
         </div>
       </main>
-      <Footer/>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
