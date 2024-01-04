15f6f21dc8d38ca61441b4009ebc0f2577ed50de
franzsamilo <franzeliezer.samilo-22@cpu.edu.ph>
2024-01-04 22:19:37 +0800

    type assertions + ignores

--- server/poolDB/flexiplanner.ts
+++ server/poolDB/flexiplanner.ts
@@ -1,15 +1,14 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-import dotenv from 'dotenv';
+import * as dotenv from 'dotenv';
 import { Pool } from 'pg';
 
 dotenv.config();
 
 const flexiplannerDB = new Pool({
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  host: process.env.DATABASE_HOST,
-  port: process.env.DATABASE_PORT,
-  database: process.env.DATABASE_NAME,
+  user: process.env.DATABASE_USER as string,
+  password: process.env.DATABASE_PASSWORD as string,
+  host: process.env.DATABASE_HOST as string,
+  port: process.env.DATABASE_PORT as unknown as number,
+  database: process.env.DATABASE_NAME as string,
   ssl: {
     rejectUnauthorized: false,
   },
