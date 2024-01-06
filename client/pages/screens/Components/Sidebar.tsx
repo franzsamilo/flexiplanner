6ad84ae8a89ec1737c207cce7dfcd3a31815a08c
franzsamilo <franzeliezer.samilo-22@cpu.edu.ph>
2024-01-06 17:26:11 +0800

    [Adjustment][Sam] Set Refresh after logout

--- client/pages/screens/Components/Sidebar.tsx
+++ client/pages/screens/Components/Sidebar.tsx
@@ -28,6 +28,7 @@ function Sidebar({ updateHeader }: SidebarProps) {
 
     if (response.ok) {
       setUser(null);
+      window.location.reload();
     } else {
       console.error('Logout failed');
     }
