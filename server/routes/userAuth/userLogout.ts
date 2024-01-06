ecd9d10dde5cd86415097c6882e0350de43d5120
franzsamilo <franzeliezer.samilo-22@cpu.edu.ph>
2024-01-06 14:26:27 +0800

    test workflow

--- server/routes/userAuth/userLogout.ts
+++ server/routes/userAuth/userLogout.ts
@@ -5,7 +5,6 @@ const router = express.Router();
 router.post("/logout", (req, res) => {
  res.clearCookie("sessionToken");
 
- res.json({ message: "User logged out" });
+ res.json({ message: "User Logged Out" });
 });
-
 export default router;
