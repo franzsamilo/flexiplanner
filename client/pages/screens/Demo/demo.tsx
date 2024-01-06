08b88e40afae758bcba55466c22f5ae76d6c1d88
franzsamilo <franzeliezer.samilo-22@cpu.edu.ph>
2024-01-06 15:09:53 +0800

    test workflow #1

--- client/pages/screens/Demo/demo.tsx
+++ client/pages/screens/Demo/demo.tsx
@@ -10,7 +10,7 @@ function DemoPage () {
 
       <main className='from-white via-blue-100 to-blue-300'>
         <div className='mx-auto my-[10px] w-[600px] '>
-          <p className='font-raleway font-extrabold text-[#425dc8] text-5xl text-center  leading-[1.25]'>
+          <p className='font-raleway font-extrabold text-[#425dc8] text-5xl text-center leading-[1.25]'>
             Demo
           </p>
         </div>
