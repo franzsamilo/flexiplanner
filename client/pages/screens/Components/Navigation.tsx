35948e7f4fcd6bb9f3a2e34333b56085142a00fc
franzsamilo <franzeliezer.samilo-22@cpu.edu.ph>
2024-01-11 13:58:23 +0800

    [Design Patterns][Sam] Navigation

--- client/pages/screens/Components/Navigation.tsx
+++ client/pages/screens/Components/Navigation.tsx
@@ -1,47 +1,146 @@
-import { useRouter } from 'next/router'
+import { useRouter, NextRouter } from 'next/router';
 
-function useNavigation () {
-  const router = useRouter()
-  function navigateTo (path: string) {
-    router.push(path)
-  }
-
-  function ToHome () {
-    navigateTo('/screens/Home/Home')
-  }
-
-  function ToMain () {
-    navigateTo('/screens/Main/Main')
-  }
-
-  function ToAboutUs () {
-    navigateTo('/screens/AboutUs/aboutUs')
-  }
-
-  function ToDemo () {
-    navigateTo('/screens/Demo/demo')
-  }
-
-  function ToLogin () {
-    navigateTo('/screens/Login/login')
-  }
-
-  function ToSignUp () {
-    navigateTo('/screens/SignUp/SignUp')
-  }
-  function ToScheduler () {
-    navigateTo('/screens/Scheduler/Scheduler')
-  }
-
-  return {
-    ToHome,
-    ToMain,
-    ToAboutUs,
-    ToDemo,
-    ToLogin,
-    ToSignUp,
-    ToScheduler
-  }
+interface Command {
+ execute(): void;
 }
 
-export default useNavigation
+class HomeCommand implements Command {
+ constructor(private router: NextRouter) {}
+
+ execute() {
+   this.router.push('/screens/Home/Home');
+ }
+}
+
+class MainCommand implements Command {
+ constructor(private router: NextRouter) {}
+
+ execute() {
+   this.router.push('/screens/Main/Main');
+ }
+}
+
+class AboutUsCommand implements Command {
+ constructor(private router: NextRouter) {}
+
+ execute() {
+   this.router.push('/screens/AboutUs/aboutUs');
+ }
+}
+
+class DemoCommand implements Command {
+ constructor(private router: NextRouter) {}
+
+ execute() {
+   this.router.push('/screens/Demo/demo');
+ }
+}
+
+class LoginCommand implements Command {
+ constructor(private router: NextRouter) {}
+
+ execute() {
+   this.router.push('/screens/Login/login');
+ }
+}
+
+class SignUpCommand implements Command {
+ constructor(private router: NextRouter) {}
+
+ execute() {
+   this.router.push('/screens/SignUp/SignUp');
+ }
+}
+
+class SchedulerCommand implements Command {
+ constructor(private router: NextRouter) {}
+
+ execute() {
+   this.router.push('/screens/Scheduler/Scheduler');
+ }
+}
+
+class NavigationFactory {
+ private router: NextRouter;
+
+ constructor(router: NextRouter) {
+   this.router = router;
+ }
+
+ createHomeCommand(): Command {
+   return new HomeCommand(this.router);
+ }
+
+ createMainCommand(): Command {
+   return new MainCommand(this.router);
+ }
+
+ createAboutUsCommand(): Command {
+   return new AboutUsCommand(this.router);
+ }
+
+ createDemoCommand(): Command {
+   return new DemoCommand(this.router);
+ }
+
+ createLoginCommand(): Command {
+   return new LoginCommand(this.router);
+ }
+
+ createSignUpCommand(): Command {
+   return new SignUpCommand(this.router);
+ }
+
+ createSchedulerCommand(): Command {
+   return new SchedulerCommand(this.router);
+ }
+}
+
+function useNavigation() {
+ const router = useRouter();
+ const navigationFactory = new NavigationFactory(router);
+
+ function navigateTo(command: Command) {
+   command.execute();
+ }
+
+ function ToHome() {
+   navigateTo(navigationFactory.createHomeCommand());
+ }
+
+ function ToMain() {
+   navigateTo(navigationFactory.createMainCommand());
+ }
+
+ function ToAboutUs() {
+   navigateTo(navigationFactory.createAboutUsCommand());
+ }
+
+ function ToDemo() {
+   navigateTo(navigationFactory.createDemoCommand());
+ }
+
+ function ToLogin() {
+   navigateTo(navigationFactory.createLoginCommand());
+ }
+
+ function ToSignUp() {
+   navigateTo(navigationFactory.createSignUpCommand());
+ }
+
+ function ToScheduler() {
+   navigateTo(navigationFactory.createSchedulerCommand());
+ }
+
+ return {
+   ToHome,
+   ToMain,
+   ToAboutUs,
+   ToDemo,
+   ToLogin,
+   ToSignUp,
+   ToScheduler
+ };
+}
+
+export default useNavigation;
